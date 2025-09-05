3ae3d024325727dbafb8265f6fbfdb8b2bf8c837
Your Name <your.email@example.com>
2025-09-05 09:24:54 -0500

    Revised the view  by moving the  toolbar out of the display components and into the  app component.

--- simple-multi-brand-solution/src/app/app.ts
+++ simple-multi-brand-solution/src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrandRxjsDisplayComponent } from './brand-rxjs-display/brand-rxjs-display.component';
 import { BrandSignalsDisplayComponent } from './brand-signals-display/brand-signals-display.component';
 import { ThemeService } from './core/theme.service';
@@ -8,10 +9,18 @@ import { BrandService } from './core/brand.service';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BrandRxjsDisplayComponent, BrandSignalsDisplayComponent],
-  templateUrl: './app.html'
+  imports: [
+    RouterOutlet,
+    BrandRxjsDisplayComponent,
+    BrandSignalsDisplayComponent,
+    MatToolbarModule,
+  ],
+  templateUrl: './app.html',
+  styleUrls: ['./app.scss'],
 })
 export class AppComponent implements OnInit {
+  brand = this.brandService.brand;
+
   constructor(
     private themeService: ThemeService,
     private route: ActivatedRoute,
@@ -19,7 +28,7 @@ export class AppComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.route.queryParams.subscribe(params => {
+    this.route.queryParams.subscribe((params) => {
       const brandId = params['brand'] || 'acme';
       this.brandService.loadBrand(brandId);
     });
